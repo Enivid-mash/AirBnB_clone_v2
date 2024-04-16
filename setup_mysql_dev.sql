7a2aa19e6cbab23f6ffabf2cc233b0788c1e63c5
yacine-pc <yacine23i@hotmail.com>
2024-04-16 21:35:44 +0100

    documented the scipt

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,14 @@
+-- this script prepares a MySQL server for the project
+
+-- create the database if not exosts
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- create a user if not exists
 CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
+-- grant all previlages to the user hbnb_dev on the database hbnb_dev_db
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO hbnb_dev@localhost;
+-- apply changes
+FLUSH PRIVILEGES;
+-- grant select previlage to the user hbnb_dev on the database performance_schema
 GRANT SELECT ON performance_schema.* TO hbnb_dev@localhost;
+-- save apply changes
 FLUSH PRIVILEGES;
