914ff9cdff4cf2b49955453239c7f1115a26a92b
yacine-pc <yacine23i@hotmail.com>
2024-04-16 21:53:22 +0100

    added the right names

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,14 +1,14 @@
 -- this script prepares a MySQL server for the project
 
 -- create the database if not exosts
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 -- create a user if not exists
-CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
 -- grant all previlages to the user hbnb_dev on the database hbnb_dev_db
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO hbnb_dev@localhost;
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO hbnb_test@localhost;
 -- apply changes
 FLUSH PRIVILEGES;
 -- grant select previlage to the user hbnb_dev on the database performance_schema
-GRANT SELECT ON performance_schema.* TO hbnb_dev@localhost;
+GRANT SELECT ON performance_schema.* TO hbnb_test@localhost;
 -- save apply changes
 FLUSH PRIVILEGES;
