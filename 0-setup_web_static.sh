1dc9439a1f15e1b780db0ea0cae4b0d2c3f9bc80
Mawunyo on wsl2 <dmashigbi@gmail.com>
2024-05-04 00:59:37 +0100

    Amend 0-setup_web_static.sh to include proper html content

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,7 +7,13 @@ sudo apt-get update -y && sudo apt-get install nginx -y
 mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
 
 # Set index.html page
-echo "<h1>ALX SE School</h1>" | sudo tee /data/web_static/releases/test/index.html
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
 # Link releases/test to current symlink
 sudo ln -sfT /data/web_static/releases/test/ /data/web_static/current
@@ -16,7 +22,7 @@ sudo ln -sfT /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
 
 # Edit Nginx config
-sudo sed -i '$ i\    location ~ /hbnb_static { \n        alias /data/web_static/current/ ;\n    }\n' /etc/nginx/sites-available/default
+sed -i "s/^\}$/\tlocation \/hbnb_static\/ \{\n\t\talias \/data\/web_static\/current\/\;\n\t\}\n\}/" /etc/nginx/sites-available/default
 
 # Reload Nginx service
 sudo service nginx restart
